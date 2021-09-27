8b067b50f9e15139f1c6708aa5b78ccfac9493a2
stevan-aspot <stevan@amplespot.com>
2021-09-27 15:58:20 +0100

    init

---
+++ TrickingLibrary.Api/Controllers/Home.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            Console.WriteLine("Index controller got request");
+            return "Hello World";
+        }
+    }
+}
